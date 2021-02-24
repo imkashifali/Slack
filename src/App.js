301e692cf3bcc20e444dacad847de5a82b90b591
kashifshafati <shafatkashif@gmail.com>
2021-02-24 17:22:01 +0500

    sidebar complete

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Header from "./components/Header.js";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+
+import styled from "styled-components";
+
 const App = () => {
   return (
     <div className="app">
       <Router>
         <>
-          <Switch>
-            <Route path="/" exact>
-              <Header />
-            </Route>
-          </Switch>
+          <Header />
+          <AppBody>
+            <Sidebar />
+
+            <Switch>
+              <Route path="/" exact>
+                {/* <Chat /> */}
+              </Route>
+            </Switch>
+          </AppBody>
         </>
       </Router>
     </div>
@@ -18,3 +27,8 @@ const App = () => {
 };
 
 export default App;
+
+const AppBody = styled.div`
+  display: flex;
+  height: 100vh;
+`;
--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,17 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDZCrdgAeuEuvHW7JTlbJoWhvnVHCHwjOc",
-  authDomain: "linked-in-8202f.firebaseapp.com",
-  projectId: "linked-in-8202f",
-  storageBucket: "linked-in-8202f.appspot.com",
-  messagingSenderId: "773262982450",
-  appId: "1:773262982450:web:9b92e5a49d490c32caeb2b",
-  measurementId: "G-FN6NQXZJWN",
+  apiKey: "AIzaSyDzeEGPW9ho-cUM4dMV-024DEbXXfx8osI",
+  authDomain: "slack-faba9.firebaseapp.com",
+  projectId: "slack-faba9",
+  storageBucket: "slack-faba9.appspot.com",
+  messagingSenderId: "98446113599",
+  appId: "1:98446113599:web:89b960c652042dbe02718a",
+  measurementId: "G-ZZ1XXHFC41",
 };
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
 const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
 
-export { db, auth };
+export { db, auth, provider };
